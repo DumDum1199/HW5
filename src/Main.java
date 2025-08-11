5145ecb95960905342ca0acd885adcfcd33d95d7
Dmitrii <bobrovdmitrii00@mail.ru>
2025-08-11 08:42:58 +0300

    HW5

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,111 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Задача 1//
+        short clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        else{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Задача 2//
+        short clientOS2 = 1;
+        int clientDeviceYear = 2014;
+        if (clientOS2 == 0) {
+            if (clientDeviceYear <2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+
+        }
+        else if (clientDeviceYear <2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Задача 3//
+        int year = 1984;
+        boolean a = (year-1584)%100==0;
+        boolean b = (year-1584)%400==0;
+        boolean c = (year-1584)%4==0;
+        if (a==true){
+           if (b==true){
+               System.out.println("Год високосный");
+           }
+           else {
+               System.out.println("Год не високосный");
+           }
+        }
+        else if(c==true){
+            System.out.println("Год високосный");
+        }
+        else{
+            System.out.println("Год не високосный");
+        }
+
+        // Задача 4//
+        int deliveryDistance = 19;
+        if (deliveryDistance<=20){
+            System.out.println("Потребуется дней: 1");
+        }
+        else if (deliveryDistance<=60) {
+            System.out.println("Потребуется дней: 2");
+        }
+        else if (deliveryDistance<=100){
+            System.out.println("Потребуется дней: 3");
+        }
+        else{
+            System.out.println("Доставка не осуществляется");
+        }
+
+        //Задача 5//
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+                System.out.println("Сейчас зима");
+                break;
+            case 2:
+                System.out.println("Сейчас зима");
+                break;
+            case 12:
+                System.out.println("Сейчас зима");
+                break;
+            case 3:
+                System.out.println("Сейчас весна");
+                break;
+            case 4:
+                System.out.println("Сейчас весна");
+                break;
+            case 5:
+                System.out.println("Сейчас весна");
+                break;
+            case 6:
+                System.out.println("Сейчас лето");
+                break;
+            case 7:
+                System.out.println("Сейчас лето");
+                break;
+            case 8:
+                System.out.println("Сейчас лето");
+                break;
+            case 9:
+                System.out.println("Сейчас осень");
+                break;
+            case 10:
+                System.out.println("Сейчас осень");
+                break;
+            case 11:
+                System.out.println("Сейчас осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет");
+        }
+
     }
 }
